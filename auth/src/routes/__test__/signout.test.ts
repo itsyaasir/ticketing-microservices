a444cc838d77a361365f14f1ec283e46975d5398
Yasir <goodwonder5@gmail.com>
2021-04-17 07:21:53 +0300

    setup helper function for auth and test for cookies

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -15,6 +15,5 @@ it("clears the cookie after signing out", async () => {
         .send({})
         .expect(200);
 
-    console.log(response.get("Set-Cookie"));
-    expect(response.get("Set-Cookie")[0]).toEqual(" 'express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly'")
+    expect(response.get("Set-Cookie")).toBeDefined()
 })
--- auth/test/setup.ts
+++ auth/test/setup.ts
@@ -1,22 +1,27 @@
+// import { request } from "express";
 import { MongoMemoryServer } from "mongodb-memory-server";
-// import { app } from "../src/app";
+import { app } from "../src/app";
+import request from 'supertest'
 import mongoose from "mongoose";
-let mongo:any
+let mongo: any
+
+// Declare global variable
+declare global { namespace NodeJS { interface Global { signin(): Promise<string[]> } } }
 beforeAll(async () => {
-    process.env.JWT_KEY="asdf";
+    process.env.JWT_KEY = "asdf";
     mongo = new MongoMemoryServer();
     const mongoUri = await mongo.getUri();
 
     await mongoose.connect(mongoUri, {
-        useNewUrlParser:true,
-        useUnifiedTopology:true
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     });
 });
 
 beforeEach(async () => {
     const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections){
+    for (let collection of collections) {
         await collection.deleteMany({});
     };
 });
@@ -26,3 +31,19 @@ afterAll(async () => {
 
     await mongoose.connection.close()
 })
+
+global.signin = async () => {
+    const email = "test@test.com"
+    const password = "password"
+
+    const response = await request(app)
+        .post("/api/users/signup")
+        .send({
+            email, password
+        })
+        .expect(201);
+
+    const cookie = response.get("Set-Cookie")
+
+    return cookie;
+}
