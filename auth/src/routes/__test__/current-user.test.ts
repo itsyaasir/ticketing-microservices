d518382b30922e383f2624a0b8e5d0079cf4bc0c
Yasir <goodwonder5@gmail.com>
2021-04-17 07:23:29 +0300

    fixed miswritten code

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -15,11 +15,11 @@ it("responds with details about the current user", async () => {
 
 });
 
-it("responds with null if not authenticated"),async () => {
+it("responds with null if not authenticated",async () => {
     const response = await request(app)
     .get("/api/users/currentuser")
     .send()
     .expect(200);
 
     expect(response.body.currentUser).toEqual(null);
-}
+})
