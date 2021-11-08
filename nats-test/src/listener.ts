1875253d2b330d8259b02d484f272053e76cd062
Yasir Shariff <goodwonder5@gmail.com>
2021-11-08 12:20:23 +0300

    added comments and fixing concurrency issues

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -8,12 +8,31 @@ const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
 stan.on("connect", () => {
     console.log("Listener connected to NATS")
 
-    const subscription = stan.subscribe("ticket:created");   //subscribe to the ticket:created topic
+    stan.on("close", () => {
+        console.log("NATS connection close!");
+        process.exit();
+    });  // close connection to nats server
+    // Options
+    const options = stan
+        .subscriptionOptions()  // create subscription options
+        .setManualAckMode(true);  // set manual ack mode
+
+    const subscription = stan.subscribe("ticket:created", "orders-service-queue-group", options);   //subscribe to the ticket:created topic with the orders-service-queue-group
     subscription.on("message", (msg: Message) => {
         const data = msg.getData(); //get the data from the message
 
         if (typeof data === "string") {
-            console.log(`Received event #${msg.getSequence()}`);
+            console.log(`Received event #${msg.getSequence()} with data ${data}`);
         }
+        msg.ack();  //acknowledge the message
     });
-})
+});
+// Interrupt Message Listener
+process.on("SIGINT", () => stan.close()); // close connection to nats server on SIGINT
+process.on("SIGTERM", () => stan.close()); // close connection to nats server on SIGTERM
+
+// Core concurrency Issues
+// 1. Listener is not processing messages
+// 2. One listener might be processing messages faster than another
+// 3. NATS might think a client is still alive when it is dead
+// 4. We might recieve the same event twice
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,20 +1,20 @@
 import nats from "node-nats-streaming"
 console.clear()
 
-const stan = nats.connect("ticketing", "abc", {
+const stan = nats.connect("ticketing", "abc", {  // nats-streaming
     url: "https://localhost:4222"
-});
+}); // connect to nats server
 
-stan.on("connect", () => {
+stan.on("connect", () => {  // listen to connect event
     console.log("Publisher connected to NATS")
 
     const data = JSON.stringify({
         id: "123",
         title: "concert",
         price: 20
-    });
+    });  // create a json object
 
     stan.publish("ticket:created", data, () => {
         console.log("Event Published");
-    })
+    }); // publish event
 });
