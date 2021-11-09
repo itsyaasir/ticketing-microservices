784652b9f6b1cfc3b9748b56f88303ce54bcbfdd
Yasir Shariff <goodwonder5@gmail.com>
2021-11-09 12:47:25 +0300

    made custom publisher and refactored code

---
+++ nats-test/src/events/ticket-created-publisher.ts
@@ -0,0 +1,9 @@
+import { Publisher } from "./base-publisher";
+import { Subjects } from "./subjects";
+import { TicketCreatedEvent } from "./ticket-created-event";
+
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+    
+}
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,20 +1,32 @@
 import nats from "node-nats-streaming"
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 console.clear()
 
 const stan = nats.connect("ticketing", "abc", {  // nats-streaming
     url: "https://localhost:4222"
 }); // connect to nats server
 
-stan.on("connect", () => {  // listen to connect event
+stan.on("connect", async () => {  // listen to connect event
     console.log("Publisher connected to NATS")
+    const publisher = new TicketCreatedPublisher(stan);
+    try {
+        await publisher.publish({
+            id: '123',
+            title: "concert",
+            price: 20
+        });
+    } catch (e) {
+        console.error(e);
+    }
+    // const data = JSON.stringify({
+    //     id: "123",
+    //     title: "concert",
+    //     price: 20
+    // });  // create a json object
 
-    const data = JSON.stringify({
-        id: "123",
-        title: "concert",
-        price: 20
-    });  // create a json object
+    // stan.publish("ticket:created", data, () => {
+    //     console.log("Event Published");
+    // }); // publish event
 
-    stan.publish("ticket:created", data, () => {
-        console.log("Event Published");
-    }); // publish event
+
 });
